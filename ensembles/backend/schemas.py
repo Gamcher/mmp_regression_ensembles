bae2d80941a09ac0eb4afd2a2f07248881edf44b
Gamcher <maryasovhimsefl2004@gmail.com>
2024-12-20 17:54:26 +0300

    ExperimentConfig is made

--- ensembles/backend/schemas.py
+++ ensembles/backend/schemas.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pydantic import BaseModel
+from typing import Optional, Union, Literal
 
 
 class ExistingExperimentsResponse(BaseModel):
@@ -16,3 +17,26 @@ class ExistingExperimentsResponse(BaseModel):
     location: Path
     experiment_names: list[str] = []
     abs_paths: list[Path] = []
+
+
+class ExperimentConfig(BaseModel):
+    """
+    Config for experiments.
+
+    Attributes:
+        name (str): The name of experiment.
+        ml_model (str): The name of model which will used in experiment.
+        n_estimators (int): The count of trees in models. Defaults to 100.
+        max_depth (int): The maximum of depth for each tree in model. Defaults to 3.
+        max_features (int/"log2"/"sqrt"): The number of features to be selected at each step for each tree in the model.
+        learning_rate (float): The learning rate of model. Defaults to 0.1.
+        target_column (str): The name of target columns in data.
+    """
+
+    name: str
+    ml_model: Literal["Gradient Boosting", "Random Forest"]
+    n_estimators: Optional[int] = 100
+    max_depth: Optional[int] = 3
+    max_features: Union[int, str]
+    learning_rate: Optional[float] = 0.1
+    target_column: str
